72d0757c5f554ce6d6157fc1fdfc8a7adad9d950
HGC-NMDS-SRE <nmds_admin@hgc.com.hk>
2025-01-13 16:35:17 +0800

    Update convert_csv_to_ini4.py

--- convert_csv_to_ini4.py
+++ convert_csv_to_ini4.py
@@ -8,8 +8,6 @@ print(df.columns)
 
 # Open the output file
 with open('inventory.ini', 'w') as f:
-    # Write the inventory groups and hosts
-    for group in df['DeviceType'].unique():
-        f.write(f'[{group}]\n')
-        for host in df[df['DeviceType'] == group]['IP']:
-            f.write(f'{host}\n')
+    # Write all hosts without grouping
+    for host in df['IP']:
+        f.write(f'{host}\n')
